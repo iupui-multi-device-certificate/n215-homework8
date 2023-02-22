3d33d1b6956d9d6556c188dd8f5b348c43be1153
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-02-22 10:47:52 -0500

    clean up

--- app/app.js
+++ app/app.js
@@ -1,22 +1,6 @@
 import { homeContent } from "../app/views/home.js";
 import { blogContent } from "../app/views/blog.js";
 
-const paragraph = `<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem id, cumque aperiam tempora necessitatibus facilis earum explicabo minima commodi ut dicta molestiae quis neque eveniet odit, odio blanditiis impedit enim!</p>`;
-
-// nice to use variable, but not ready for a templating system
-// function setTitle(pageName) {
-//   return `<h1>${pageName}</h1>`;
-// }
-
-/*
-<div class="images">
-  <img src="" alt="" />
-  <img src="" alt="" />
-  <img src="" alt="" />
-</div>;
-
-*/
-
 function initListeners() {
   $("nav a").click(function (e) {
     let aID = e.currentTarget.id;
@@ -29,5 +13,4 @@ function initListeners() {
 $(document).ready(function () {
   initListeners();
   $("#app").html(homeContent);
-  // $("#app").html(setTitle("home"));
 });
