036ff5d30aaba0df3cf783065a42e011dbd424e2
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-02-22 12:21:27 -0500

    fix blog-post header styling

--- app/views/blog.js
+++ app/views/blog.js
@@ -20,7 +20,7 @@ export const blogContent = () => `
           <article class="blog-post">
           <img src="https://source.unsplash.com/random/200x200" alt="">
           <div class="blog-post-body">
-          <h1>Article 2</h1>
+          <h1 class="blog-post-title">Article 2</h1>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
                 id, cumque aperiam tempora necessitatibus facilis earum explicabo
@@ -32,7 +32,7 @@ export const blogContent = () => `
           <article class="blog-post">
           <img src="https://source.unsplash.com/random/200x200" alt="">
           <div class="blog-post-body">
-          <h1>Article 3</h1>
+          <h1 class="blog-post-title">Article 3</h1>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
                 id, cumque aperiam tempora necessitatibus facilis earum explicabo
@@ -44,7 +44,7 @@ export const blogContent = () => `
           <article class="blog-post">
           <img src="https://source.unsplash.com/random/200x200" alt="">
           <div class="blog-post-body">
-          <h1>Article 4</h1>
+          <h1 class="blog-post-title">Article 4</h1>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
                 id, cumque aperiam tempora necessitatibus facilis earum explicabo
@@ -56,7 +56,7 @@ export const blogContent = () => `
           <article class="blog-post">
           <img src="https://source.unsplash.com/random/200x200" alt="">
           <div class="blog-post-body">
-          <h1>Article 5</h1>
+          <h1 class="blog-post-title">Article 5</h1>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
                 id, cumque aperiam tempora necessitatibus facilis earum explicabo
