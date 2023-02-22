e6c57a24997d2b0ea24c8098eb5835a03a668a73
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-02-22 06:52:50 -0500

    start blog

---
+++ app/views/blog.js
@@ -0,0 +1,10 @@
+// export const blog = () => `
+
+// <section class="blog">
+// <article>
+
+// <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem id, cumque aperiam tempora necessitatibus facilis earum explicabo minima commodi ut dicta molestiae quis neque eveniet odit, odio blanditiis impedit enim!</p>
+
+// </section>
+
+// `;
