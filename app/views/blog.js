2eef182528354dc8efdb77005a7a9d630a1334f0
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-02-22 17:59:46 -0500

    reduce blog to 3 articles

--- app/views/blog.js
+++ app/views/blog.js
@@ -41,30 +41,6 @@ export const blogContent = () => `
               </p>
           </div>
           </article>
-          <article class="blog-post">
-          <img src="https://source.unsplash.com/random/200x200" alt="">
-          <div class="blog-post-body">
-          <h1 class="blog-post-title">Article 4</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-                id, cumque aperiam tempora necessitatibus facilis earum explicabo
-                minima commodi ut dicta molestiae quis neque eveniet odit, odio
-                blanditiis impedit enim!
-              </p>
-          </div>
-          </article>
-          <article class="blog-post">
-          <img src="https://source.unsplash.com/random/200x200" alt="">
-          <div class="blog-post-body">
-          <h1 class="blog-post-title">Article 5</h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-                id, cumque aperiam tempora necessitatibus facilis earum explicabo
-                minima commodi ut dicta molestiae quis neque eveniet odit, odio
-                blanditiis impedit enim!
-              </p>
-          </div>
-          </article>
         </section>
 
 `;
