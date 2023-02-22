844a9cfaaa406b1185f845ecc05562c60d041381
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-02-22 08:59:06 -0500

    add rest of image placeholders

--- app/views/blog.js
+++ app/views/blog.js
@@ -16,39 +16,51 @@ export const blog = () => `
           </article>
           <article class="blog-post">
             <h1>Article 2</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-              id, cumque aperiam tempora necessitatibus facilis earum explicabo
-              minima commodi ut dicta molestiae quis neque eveniet odit, odio
-              blanditiis impedit enim!
-            </p>
+            <div class="blog-post-body">
+            <img src="https://source.unsplash.com/random/200x200" alt="">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+                id, cumque aperiam tempora necessitatibus facilis earum explicabo
+                minima commodi ut dicta molestiae quis neque eveniet odit, odio
+                blanditiis impedit enim!
+              </p>
+          </div>
           </article>
           <article class="blog-post">
             <h1>Article 3</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-              id, cumque aperiam tempora necessitatibus facilis earum explicabo
-              minima commodi ut dicta molestiae quis neque eveniet odit, odio
-              blanditiis impedit enim!
-            </p>
+            <div class="blog-post-body">
+            <img src="https://source.unsplash.com/random/200x200" alt="">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+                id, cumque aperiam tempora necessitatibus facilis earum explicabo
+                minima commodi ut dicta molestiae quis neque eveniet odit, odio
+                blanditiis impedit enim!
+              </p>
+          </div>
           </article>
           <article class="blog-post">
             <h1>Article 4</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-              id, cumque aperiam tempora necessitatibus facilis earum explicabo
-              minima commodi ut dicta molestiae quis neque eveniet odit, odio
-              blanditiis impedit enim!
-            </p>
+            <div class="blog-post-body">
+            <img src="https://source.unsplash.com/random/200x200" alt="">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+                id, cumque aperiam tempora necessitatibus facilis earum explicabo
+                minima commodi ut dicta molestiae quis neque eveniet odit, odio
+                blanditiis impedit enim!
+              </p>
+          </div>
           </article>
           <article class="blog-post">
             <h1>Article 5</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-              id, cumque aperiam tempora necessitatibus facilis earum explicabo
-              minima commodi ut dicta molestiae quis neque eveniet odit, odio
-              blanditiis impedit enim!
-            </p>
+            <div class="blog-post-body">
+            <img src="https://source.unsplash.com/random/200x200" alt="">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+                id, cumque aperiam tempora necessitatibus facilis earum explicabo
+                minima commodi ut dicta molestiae quis neque eveniet odit, odio
+                blanditiis impedit enim!
+              </p>
+          </div>
           </article>
         </section>
 
